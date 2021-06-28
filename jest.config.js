f2434f656a04064fe76f4a010568b1fd0d7eed4d
Bryn <bryn.discord@gmail.com>
2021-06-27 19:28:26 -0600

    refactor(configs): update eslint, package, tscong

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: MIT OR CC0-1.0
-Bryn (Safire Project) */
-
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/types/'],
-};
